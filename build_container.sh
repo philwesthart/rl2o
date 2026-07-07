ddb3982219126e9b697f636f6d05d955b91ba156
Phil Westhart <philwesthart@gmail.com>
2026-07-07 11:53:30 -0500

    Updating container helper scripts

---
+++ build_container.sh
@@ -0,0 +1,1 @@
+docker build . -t rl2o:latest
--- start_container.sh
+++ start_container.sh
@@ -3,7 +3,7 @@ docker run \
     -it \
     --network host \
     --cap-add NET_ADMIN \
-    --device=/dev/video0 \
-    --device=/dev/video1 \
     -v $(pwd):/app \
-    telemetry
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+    rl2o
