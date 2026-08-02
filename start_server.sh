54726b5b4a5c3738e83808f65b0f363d4ef3ee9c
Phil Westhart <philwesthart@gmail.com>
2026-08-02 16:23:27 -0500

    Removing pathing to uvicorn
    This should be installed system wide, or installed in venv. Make sure to activate venv
    
    source ./venv/bin/activate

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-./venv/bin/uvicorn telemetry_server:app --host 0.0.0.0 --port 8000 --no-access-log
+uvicorn telemetry_server:app --host 0.0.0.0 --port 8000 --no-access-log
